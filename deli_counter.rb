eb7a43ce2703a18e59263ee4a90d2e8492fb9a09
Janis Lebron <janislebron@gmail.com>
2020-05-17 20:52:28 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -6,5 +6,13 @@ def line(array)
 end
 
 def take_a_number(array, name)
+  puts "#{name} #{array}"
+end
 
+def now_serving
+  if 
+    return
+  else
+    return "There is nobody waiting to be served!"
+  end
 end
